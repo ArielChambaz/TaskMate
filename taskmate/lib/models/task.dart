95b3882f969256e0a969d6a4cf678d21e24cf1b7
Ariel <ariel.chambaz@epitech.eu>
2025-04-14 18:32:32 +0700

    (+) Add database

--- taskmate/lib/models/task.dart
+++ taskmate/lib/models/task.dart
@@ -1,6 +1,25 @@
 class Task {
+  int? id; // Ajout de l'ID
   String title;
   bool isCompleted;
 
-  Task({required this.title, this.isCompleted = false});
+  Task({this.id, required this.title, this.isCompleted = false});
+
+  // Convertir un objet Task en Map pour SQLite
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'isCompleted': isCompleted ? 1 : 0,
+    };
+  }
+
+  // Convertir une Map SQLite en objet Task
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(
+      id: map['id'],
+      title: map['title'],
+      isCompleted: map['isCompleted'] == 1,
+    );
+  }
 }
